bba38b4051a2727fa7a9208b732142e00590b14f
KillingForm <asdf>
2022-04-07 23:03:00 -0400

    feat: working on def's

--- philo-sophia.js
+++ philo-sophia.js
@@ -42,29 +42,42 @@ export default {
   cause: "The producer of an effect, result, or consequence.",
   explanation: "The act or process of explaining.; Something that explains.",
   "epistimology vs ontology": "e: ways we know things, o: the way things are",
-  "coherence theory of truth": "",
-  "coherence theory of justification": "",
-  "prediction vs postdiction": "",
-  pragmatist: " ",
+  "coherence theory of truth":
+    "A coherence theory of truth states that the truth of any (true) proposition consists in its coherence with some specified set of propositions.",
+  "coherence theory of epistemic justification":
+    "According to the coherence theory of justification, also known as coherentism, a belief or set of beliefs is justified, or justifiably held, just in case the belief coheres with a set of beliefs, the set forms a coherent system or some variation on these themes.",
+  prediction: "an act of predicting",
+  predicting: "to declare or indicate in advance",
+  postdiction:
+    "An assertion or deduction about something in the past; the act of making such an assertion or deduction.",
+  pragmatist:
+    "a person who takes a practical approach to problems and is concerned primarily with the success or failure of his or her actions",
   "pascals wager":
-    " It posits that humans bet with their lives that God either exists or does not.",
-  etymology: " ",
-  "leplasses demon": " ",
-  "tertiary structure": " ",
-  "reflexive?": " ",
-  "cartesian certainty": " ",
-  fatalism: " ",
-  gnosis: " ",
-  "euthepro delema?": " ",
+    "It posits that humans bet with their lives that God either exists or does not.",
+  etymology:
+    "Study of the history of linguistic forms, that is the history of how words are written and pronounced, and how their spelling and pronunciation changed",
+  "Laplace's demon": "Causal determinism by Pierre-Simon Laplace",
+  "Causal Determinism":
+    "historical determinism; the idea that every event is necessitated by antecedent events and conditions together with the laws of nature",
+  "tertiary structure":
+    "the three-dimensional structure of a protein—is the next level of complexity in protein folding. Whereas individual amino acids in the primary sequence can interact with one another to form secondary structures such as helices and sheets, and individual amino acids from distant parts of the primary sequence can intermingle via charge-charge, hydrophobic, disulfide, or other interactions, the formation of these bonds and interactions serve to change the shape of the overall protein. The folding that we end up with for a given polypeptide is the tertiary structure.",
+  reflexive: "directed or turned back on itself",
+  cartesianism:
+    "species of rationalism, because Cartesians hold that knowledge—indeed, certain knowledge—can be derived through reason from innate ideas.",
+  fatalism:
+    "The doctrine that all events are predetermined by fate and are therefore unalterable.",
+  gnosis:
+    "Intuitive apprehension of spiritual truths, an esoteric form of knowledge sought by the Gnostics.",
+  "euthyphro dilemma": " ",
   structuralist: "",
   "abductive vs deductive": "",
   "abduction (abductive argument)": "",
   inductive: " ",
-  contadictions: "",
-  "synthetic/analytic disctinction": "",
+  contradiction: "",
+  "synthetic/analytic distinction": "",
   "concrete objects?": "",
   "dualism vs nominalism": "",
-  "Anarcho-syndicalism": "",
+  "anarcho-syndicalism": "",
   explication: "",
   primitives: "",
   "litnus test": "",
