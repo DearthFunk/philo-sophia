ca3297835862a09a9757904aeda941e00a6c4957
KillingForm <asdf>
2022-04-21 00:59:45 -0400

    fix: renders child elements
      todo: replace w templating

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 import dictionaryJson from "./philo-sophia.js";
 
 let results = [];
+let dictionary;
 let resultsElement = document.getElementById("results");
 let searchElement = document.getElementById("search-input");
 searchElement.oninput = inputUpdated;
 
+processDictionary();
 inputUpdated(); //execute with no input results in full list results
 
 //////////////////////////////////////////////
+
+function processDictionary() {
+  dictionary = Object.keys(dictionaryJson).map((k) => ({
+    word: k,
+    definition: dictionaryJson[k],
+    foundWords: [],
+  }));
+  dictionary.forEach((item) => {
+    let dictionaryWords = Object.keys(dictionaryJson);
+    let definitionWords = item.definition.split(" ");
+    let foundWords = definitionWords.filter((word) =>
+      dictionaryWords.includes(word)
+    );
+    item.foundWords = foundWords;
+  });
+}
 
 function resetResults() {
   results = [];
@@ -15,23 +33,34 @@ function resetResults() {
 }
 
 function updateResults(searchTerm) {
-  for (const key in dictionaryJson) {
-    if (key.includes(searchTerm)) {
-      results.push({
-        title: key,
-        description: dictionaryJson[key],
-      });
-    }
-  }
+  results = dictionary.filter((definition) =>
+    definition.word.includes(searchTerm)
+  );
 }
 
 function renderResults() {
   results.forEach((item) => {
-    var li = document.createElement("li");
-    li.appendChild(
-      document.createTextNode(`${item.title}: ${item.description}`)
-    );
-    resultsElement.appendChild(li);
+    let listItem = document.createElement("li");
+    let titleElement = document.createElement("span");
+    titleElement.appendChild(document.createTextNode(`${item.word}: `));
+    listItem.appendChild(titleElement);
+    listItem.appendChild(document.createTextNode(item.definition));
+
+    if (item.foundWords.length) {
+      let descriptionList = document.createElement("dl");
+
+      item.foundWords.forEach((foundWord) => {
+        let { definition } = dictionary.find((item) => item.word === foundWord);
+        let dt = document.createElement("dt");
+        let dd = document.createElement("dd");
+        dt.appendChild(document.createTextNode(`${foundWord}: `));
+        dd.appendChild(document.createTextNode(definition));
+        descriptionList.appendChild(dt);
+        descriptionList.appendChild(dd);
+      });
+      listItem.appendChild(descriptionList);
+    }
+    resultsElement.appendChild(listItem);
   });
 }
 
